2b99cecd1f54afaf896ccd8ea5a616fe7741fefa
Ivan Kazintsev <ikazintsev@MacBook-Air-Ivan-9.local>
2023-02-14 00:20:28 +0300

    text

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,9 +22,7 @@ export const registerUser = async (req, res) => {
         const errors = validationResult(req)
         if (!errors.isEmpty()) return res.status(400).json(errors.array())
         const user = await createUser(req.body)
-
         res.status(200).json(user)
-
     } catch (err) {
         res.status(500).json(err)
     }
