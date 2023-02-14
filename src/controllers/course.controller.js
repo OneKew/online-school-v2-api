16191ca8d3624d2941074ebd9b9acf47904adbcc
OneKew <i_kazintsev@mail.ru>
2023-02-14 18:27:06 +0300

    add course routes and assignment files

---
+++ src/controllers/course.controller.js
@@ -0,0 +1,16 @@
+export let viewCourse = undefined;
+export let updateSelectedLesson = undefined;
+export let getSelectedLesson = undefined;
+export let createLesson = undefined;
+export let updateSelectedModule = undefined;
+export let getSelectedModule = undefined;
+export let createModule = undefined;
+export let updateSelectedCourse = undefined;
+export let createCourse = undefined;
+export const getCourses = (req, res) => {
+
+}
+
+export const getSelectedCourse = (req, res) => {
+
+}
--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -1,8 +1,31 @@
 import express from "express";
 import * as courseController from "../controllers/course.controller.js";
+import {checkCredentials} from "../services/auth.service.js";
 
 
-export const courseRouter = express.Router()
+export const courseRouter = express.Router();
+
+courseRouter.get('/course', checkCredentials, courseController.getCourses);
+
+courseRouter.post('/course', checkCredentials, courseController.createCourse)
+courseRouter.get('/course/:id',checkCredentials, courseController.getSelectedCourse);
+courseRouter.patch('/course/:id', checkCredentials, courseController.updateSelectedCourse)
+
+courseRouter.post('/course/:id/modules', checkCredentials, courseController.createModule)
+courseRouter.get('/course/:id/modules/:id', checkCredentials, courseController.getSelectedModule)
+courseRouter.patch('/course/:id/modules/:id', checkCredentials, courseController.updateSelectedModule)
+
+courseRouter.post('/course/:id/modules/:id/lessons', checkCredentials, courseController.createLesson)
+courseRouter.get('/course/:id/modules/:id/lessons/:id', checkCredentials, courseController.getSelectedLesson)
+courseRouter.patch('/course/:id/modules/:id/lessons/:id', checkCredentials, courseController.updateSelectedLesson)
+
+
+
+courseRouter.get('/course/:id/view', checkCredentials, courseController.viewCourse)
+
+
+
+courseRouter.post('/course');
 
 // adminRouter.post('/login', authController.login)
 //
